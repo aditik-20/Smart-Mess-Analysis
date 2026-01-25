5baa6123644d12761e58589f5ce5999c3a680e27
aditik-20 <ak.krishnaaditi@gmail.com>
2026-01-25 17:23:32 +0530

    v1.2 Updated schema.sql

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,51 +1,48 @@
-CREATE DATABASE hostel_Management;
+create database if not exists hostelmessanalysis;
+use hostelmessanalysis;
+create table hostels(
+	hostel_id int primary key auto_increment,
+    hostel_name varchar(100) not null unique
+    );
 
-use hostel_Management;
-
-CREATE TABLE hostels(
-hostel_id INT PRIMARY KEY,
-hostel_name VARCHAR(50) UNIQUE
-);
 
 create table students(
-student_id INT AUTO_INCREMENT PRIMARY KEY,
-student_name varchar(50) NOT NULL,
-hostel_id INT,
-FOREIGN KEY (hostel_id) REFERENCES hostels(hostel_id)
+student_id int primary key auto_increment,
+student_name varchar(60) not null ,
+hostel_id int not null,
+foreign key(hostel_id) references hostels(hostel_id)
 );
 
-create table attendance (
-attendance_id INT AUTO_INCREMENT PRIMARY KEY,
-student_id INT,
-FOREIGN KEY(student_id) REFERENCES students(student_id),
-date DATE NOT NULL,
-status BOOLEAN
-);
-
-create table costs (
-cost_id INT AUTO_INCREMENT PRIMARY KEY,
-date DATE NOT NULL,
-hostel_id INT,
-FOREIGN KEY (hostel_id) REFERENCES hostels(hostel_id),
-total_cost DECIMAL
-);
-
-create table meals_log (
-meal_id INT AUTO_INCREMENT PRIMARY KEY,
-student_id INT,
-FOREIGN KEY(student_id) REFERENCES students(student_id),
-type ENUM("lunch","breakfast","dinner"),
-wastage_kg DECIMAL,
-date DATE NOT NULL,
-hour INT
-);
-
-create table Wastage (
-waste_id INT AUTO_INCREMENT PRIMARY KEY,
-hostel_id INT,
-FOREIGN KEY (hostel_id) REFERENCES hostels(hostel_id),
-type ENUM("lunch","breakfast","dinner"),
-date DATE NOT NULL,
-wastage_kg DECIMAL
-);
-
+show tables;
+create table attendance(
+	attendance_id int primary key not null auto_increment ,
+    student_id int not null,
+    date DATE not null , 
+    status boolean not null , 
+    unique(student_id , date),
+    foreign key(student_id) references students(student_id)
+    );
+    
+create table meal_log(
+	meal_id int primary key not null auto_increment,
+    student_id int not null,
+    meal_type enum('Breakfast' , 'Lunch' , 'Dinner') not null , 
+    date DATE not null,
+    unique(student_id , date , meal_type),
+    foreign key(student_id) references students(student_id)
+    );
+    
+    create table wastage(
+    waste_id int primary key not null auto_increment,
+    hostel_id int not null,
+    meal_type enum('Breakfast' , 'Lunch' , 'Dinner') not null , 
+    date DATE not null,
+    waste_kg decimal(6,2) not null check (waste_kg >= 0),
+    foreign key(hostel_id) references hostels(hostel_id));
+    
+    create table costs(
+		costs_id int primary key not null auto_increment,
+        hostel_id int not null ,
+        date DATE not null,
+        total_cost decimal(12,2) not null check(total_cost >= 0),
+         foreign key(hostel_id) references hostels(hostel_id));
