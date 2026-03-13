dffde0f4601baf4d82038e3f0a6ecbaaf676d834
hardik951 <hardiksahni09@gmail.com>
2026-03-13 18:32:58 +0530

    v1.8

--- backend/sql/views.sql
+++ backend/sql/views.sql
@@ -16,3 +16,20 @@ SELECT
 FROM Meal m
 LEFT JOIN Student_Meal sm ON m.meal_id = sm.meal_id
 GROUP BY m.meal_id, m.meal_type, m.date;
+
+CREATE VIEW daily_wastage_report AS
+SELECT
+    m.date,
+    SUM(w.waste_kg) AS total_wastage
+FROM Wastage w
+JOIN Meal m ON w.meal_id = m.meal_id
+GROUP BY m.date;
+
+
+CREATE VIEW ingredient_usage AS
+SELECT
+    mn.menu_date,
+    i.ingredient_name
+FROM Menu mn
+JOIN Menu_Ingredient mi ON mn.menu_id = mi.menu_id
+JOIN Ingredient i ON mi.ingredient_id = i.ingredient_id;
